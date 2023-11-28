f2b68d2d0120c2d503a96d0247d56216ea9fe160
Jialin Weng <101737864+Jialin-weng@users.noreply.github.com>
2023-11-27 22:29:25 -0500

    loginSystemProgress

--- README.md
+++ README.md
@@ -1,27 +1,30 @@
 Article CRUD
-    Node service
-    Containerize service
+Node service
+Containerize service
 Authentication
-    Verify username & password function
-    Use a cookie or global object to track session information
-    Containerize service
+Verify username & password function
+Use a cookie or global object to track session information
+Containerize service
 Ads
-    Node service
-    Containerize service
-    Write a function to display an ad
-    Write function to track an ad view
-    Write function to track an ad click
+Node service
+Containerize service
+Write a function to display an ad
+Write function to track an ad view
+Write function to track an ad click
 Apache setup
-    Create proxy entries for node services
-    Create html files
-    Containerize
+Create proxy entries for node services
+Create html files
+Containerize
 Develop the user interface
-    Create three HTML views: reader, commenter, author
-    Create Javascript functions to change the view
-    Write a controlling function to change the views using the above functions based on the user role
-    Connect each view to live data
+Create three HTML views: reader, commenter, author
+Create Javascript functions to change the view
+Write a controlling function to change the views using the above functions based on the user role
+Connect each view to live data
 Mongo setup
-    Containerize service
-    Create data for testing
+Containerize service
+Create data for testing
 Write a test plan
 
+App Port Details
+
+Login.js: port 3003
---
+++ nodejs/login.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors'); // Import the cors middleware
+const app = express();
+const url = "mongodb://127.0.0.1:27017/";
+const client = new MongoClient(url);
+const port = 3003;
+// Use the cors middleware to enable cross-origin requests
+app.use(express.json());
+app.use(cors());
+client.connect().then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+});
+
+app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+app.get('/users', async (request, response) => {
+    try {
+        await client.connect();
+        await client.db('DailyBugle').collection('Users')
+        .find()
+        .toArray()
+        .then ( results => {
+            response.send( results);
+        })
+        .catch( error=> console.error(error));
+    } catch (error) {
+        console.error(error);
+    } finally {
+        client.close();
+    }
+});
+
+app.post('/signup', async (request, response) => {
+    try {
+        const { username, password } = request.body;
+
+        // Connect to the MongoDB database
+        await client.connect();
+
+        // Insert the new user into the 'Users' collection
+        const result = await client.db('DailyBugle').collection('Users').insertOne({
+            username,
+            password,
+        });
+
+        // Send a response indicating success
+        response.status(201).json({ message: 'User created successfully', userId: result.insertedId });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        alert(error)
+        response.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        // Close the MongoDB client connection only if it was successfully connected
+        client.close();
+        
+    }
+});
+
+
