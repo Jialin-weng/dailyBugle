555f1ac60382eeb03b8ac6e343a57e72ebdfc694
Jialin Weng <101737864+Jialin-weng@users.noreply.github.com>
2023-11-29 19:14:55 -0500

    ads view and click

--- app/controls.js
+++ app/controls.js
@@ -2,10 +2,25 @@ const endpoint = {};
 endpoint['signup']='http://localhost:3003/signup';
 endpoint['login']='http://localhost:3003/login';
 endpoint['ads'] = 'http://localhost:3004/ads';
+endpoint['adsIP'] = 'http://localhost:3004/get-ip';
+endpoint['adsClicked'] = 'http://localhost:3004/adsClicked';
+endpoint['adsViewed'] = 'http://localhost:3004/adsViewed';
+
+var ipAddress = "";
+
+const browserInfo = {
+    appName: navigator.appName,
+    appVersion: navigator.appVersion,
+    userAgent: navigator.userAgent,
+    platform: navigator.platform,
+};
+
+console.log('Browser Information:', navigator.userAgent);
 
 function logout(event){
     event.preventDefault();
     document.cookie = "userType=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "userName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.href = 'login.html';
 }
 function setCookie(name, value, days) {
@@ -31,6 +46,14 @@ function getCookie(name) {
 
 // Example of checking the 'userType' cookie
 const userType = getCookie('userType');
+const currentUserName = getCookie('userName');
+
+if (currentUserName) {
+    console.log(`Username: ${currentUserName}`);
+} else {
+    console.log('Username not found');
+}
+
 if (userType) {
     console.log(`User type: ${userType}`);
 } else {
@@ -68,8 +91,9 @@ async function submitLoginForm(event){
     var password = document.getElementById('password').value;
     var result = await loginForm(username,password);
     if (result.success == true){
-        console.log(result)
         setCookie('userType', result.userType, 7);  // Expires in 7 days
+        setCookie('userName', username, 7);  // Expires in 7 days
+
         window.location.href = 'index.html';
     }
     else if (result.success == false){
@@ -142,36 +166,168 @@ async function getAd(){
         console.error('Error fetching ads:', error);
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Make a GET request to the server endpoint
+    fetch(endpoint["adsIP"])
+        .then(response => response.text())
+        .then(data => {
+            // Log the response from the server
+            ipAddress = data;
+            console.log(ipAddress)
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
     
 async function displayAds() {
-    const ads = await getAd();
+    const adList = await getAd();
+
+    // Check if there are any ads
+    if (adList.length === 0) {
+        console.log('No ads available.');
+        return;
+    }
+    const randomIndex = Math.floor(Math.random() * adList.length);
+    const ads = adList[randomIndex];
     console.log(ads);
     const adsContainer = document.getElementById('adsContainer');
 
     // Clear previous content
     adsContainer.innerHTML = '';
+        if (!currentUserName) {
+            var sendName = "anon"
+        } else{
+            var sendName = currentUserName
+        }
 
-    ads.forEach(ad => {
+    
         const adContainer = document.createElement('div');
         adContainer.classList.add('ad-container'); // Add a class for styling
 
+
+
         // Header element
         const headerElement = document.createElement('h2');
-        headerElement.textContent = `Ad Header: ${ad.adName}`;
+        headerElement.textContent = `${ads.adName}`;
         adContainer.appendChild(headerElement);
 
         // Body element
         const bodyElement = document.createElement('p');
-        bodyElement.textContent = `Ad Body: ${ad.body}`;
+        bodyElement.textContent = `${ads.body}`;
         adContainer.appendChild(bodyElement);
 
         // Add a click event listener to the adContainer
-        adContainer.addEventListener('click', () => {
+        adContainer.addEventListener('click', async() => {
             // Handle the click event (e.g., open a detailed view, navigate to a link, etc.)
-            console.log('Ad clicked:', ad._id);
+            if (!currentUserName) {
+                var sendName = "anon"
+            } else{
+                var sendName = currentUserName
+            }
+            await sendAdClickRequest(ads._id, ipAddress,sendName);
+            
         });
+        
 
         adsContainer.appendChild(adContainer);
-    });
+        await sendAdViewedRequest(ads._id, ipAddress,sendName);
+
 }
 
+
+async function sendAdClickRequest(adId, ipAddress,sendName) {
+    const response = await fetch(endpoint['adsClicked'], {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            ad_id: adId,
+            user_id: sendName, // Implement your logic to get the user ID
+            ip_add: ipAddress,
+            user_agent: navigator.userAgent, // Example: sending user agent information
+        }),
+    });
+
+    if (response.ok) {
+        console.log('Ad click recorded successfully');
+    } else {
+        console.error('Failed to record ad click');
+    }
+}
+
+
+async function sendAdViewedRequest(adId, ipAddress,sendName) {
+    const response = await fetch(endpoint['adsViewed'], {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            ad_id: adId,
+            user_id: sendName, // Implement your logic to get the user ID
+            ip_add: ipAddress,
+            user_agent: navigator.userAgent, // Example: sending user agent information
+        }),
+    });
+
+    if (response.ok) {
+        console.log('Ad click recorded successfully');
+    } else {
+        console.error('Failed to record ad click');
+    }
+}
+
+
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Call displayAds with the result of getAd
+    displayAds(getAd());
+    const userType = getCookie('userType');
+    generateContentBasedOnUserType(userType);
+});
+
+function generateContentBasedOnUserType(userType) {
+    // Sections based on user type
+    switch (userType) {
+        case 'author':
+            // Generate content for author
+            generateAuthorContent();
+            break;
+        case 'reader':
+            // Generate content for reader
+            generateReaderContent();
+            break;
+        default:
+            // Generate content for none or other user types
+            generateDefaultContent();
+    }
+}
+function generateAuthorContent() {
+    const viewWelcome = document.getElementById('viewWelcome');
+    viewWelcome.innerHTML = '<div class="container text-center"><h2>Welcome Author!</h2><p>Your author-specific content goes here.</p></div>';
+
+    const viewChangeSection = document.getElementById('viewContent');
+    viewChangeSection.innerHTML = '<div class="container text-center"><h2>Author View - Headline Story Title</h2><p>Author-specific teaser for the headline story.</p></div>';
+}
+
+function generateReaderContent() {
+    const viewWelcome = document.getElementById('viewWelcome');
+    viewWelcome.innerHTML = '<div class="container text-center"><h2>Welcome Reader!</h2><p>Your reader-specific content goes here.</p></div>';
+
+    const viewChangeSection = document.getElementById('viewContent');
+    viewChangeSection.innerHTML = '<div class="container text-center"><h2>Reader View - Headline Story Title</h2><p>Reader-specific teaser for the headline story.</p></div>';
+}
+
+function generateDefaultContent() {
+    // Example: Display default content for none or other user types
+    const viewWelcome = document.getElementById('viewWelcome');
+    viewWelcome.innerHTML = '<div class="container text-center"><h2>Welcome!</h2><p>Your default content goes here.</p></div>';
+
+    // Insert additional content into ViewChange section
+    const viewChangeSection = document.getElementById('viewContent');
+    viewChangeSection.innerHTML = '<div class="container text-center"><h2>Default View - Headline Story Title</h2><p>Default teaser for the headline story.</p></div>';
+}
--- nodejs/ads.js
+++ nodejs/ads.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const { ObjectId } = require('mongodb');
 const cors = require('cors'); // Import the cors middleware
 const app = express();
 const url = "mongodb://127.0.0.1:27017/";
@@ -21,4 +22,72 @@ app.get("/ads", (req, res) => {
     ads.find({}).toArray().then(ads => {
         res.send(ads);
     });
+});
+
+app.get('/get-ip', (req, res) => {
+    // Get user's IP address
+    const ipAddress = req.ip || req.connection.remoteAddress;
+    res.send(ipAddress);
+});
+
+
+app.post("/adsClicked", (req, res) => {
+    const database = client.db('DailyBugle');
+    const ads = database.collection('Ads');
+
+    // Assuming req.body contains the ad_id and other relevant information
+    const { ad_id, user_id,ip_add,user_agent  } = req.body;
+
+    // Convert the ad_id to ObjectId
+    const adObjectId = new ObjectId(ad_id);
+
+    // Update the ad's clicked array
+    ads.updateOne(
+        { _id: adObjectId },
+        { $addToSet: { clickedBy: {user_id,ip_add,user_agent} } },
+        (err, result) => {
+            if (err) {
+                console.error('Error updating ad:', err);
+                res.status(500).send('Internal Server Error');
+            } else {
+                // Check if the update was successful
+                if (result.modifiedCount > 0) {
+                    res.send('Ad updated successfully');
+                } else {
+                    res.status(404).send('Ad not found');
+                }
+            }
+        }
+    );
+});
+
+
+app.post("/adsViewed", (req, res) => {
+    const database = client.db('DailyBugle');
+    const ads = database.collection('Ads');
+
+    // Assuming req.body contains the ad_id and other relevant information
+    const { ad_id, user_id,ip_add,user_agent  } = req.body;
+
+    // Convert the ad_id to ObjectId
+    const adObjectId = new ObjectId(ad_id);
+
+    // Update the ad's clicked array
+    ads.updateOne(
+        { _id: adObjectId },
+        { $addToSet: { viewedBy: {user_id,ip_add,user_agent} } },
+        (err, result) => {
+            if (err) {
+                console.error('Error updating ad:', err);
+                res.status(500).send('Internal Server Error');
+            } else {
+                // Check if the update was successful
+                if (result.modifiedCount > 0) {
+                    res.send('Ad updated successfully');
+                } else {
+                    res.status(404).send('Ad not found');
+                }
+            }
+        }
+    );
 });
